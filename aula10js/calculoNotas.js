38cc0a5b0249bc58d5e683198d985bd8f641d1ff
Thamirys Riguete <thamirys.nascimento@aluno.senai.br>
2024-08-23 15:16:38 -0300

    Adicionando cálculo de média

---
+++ aula10js/calculoNotas.js
@@ -0,0 +1,36 @@
+var alunos = ["Pedro", "Tiago", "João"]
+var notasA = [8.0, 7.0, 6.0]
+var notasB = [7.0, 5.7, 6.3]
+
+function media(nota1, nota2){
+     var n1 = nota1
+     var n2 = nota2
+     var media = (nota1 + nota2) / 2
+     return media    
+}
+ //aprovado = 7 pra cima, recuperação = entre 5 e 7, reprovado = 5 pra baixo
+
+function situacao(media){
+    if(media  >= 7){
+        return "Aprovado"
+    }
+    else if(media < 7 && media >= 5){
+        return "Recuperação"
+    }
+    else{
+        return "Reprovado"
+    }
+}
+
+
+function mostraResultados(){
+    for(var index in alunos){
+        var notaPri = notasA[index]
+        var notasSeg = notasB[index]
+        var mediaAtual = media(notaPri, notasSeg)
+        var 
+        console.log("O aluno", alunos[index], "teve média de", mediaAtual, ",E sua situação final é:", final)
+    }
+}
+ mostraResultados()
+
---
+++ aula10js/script.js
@@ -0,0 +1,19 @@
+/*console.log("Está funcionando")
+
+var array = ["Txto1", 10, 25, "Oswaldo", "Testo2"]
+
+console.log(array[0])
+
+for(var i = 0; i < 5; i++){
+    console.log(array[i])
+}
+
+for(var index in array){
+    console.log(array[index])
+}
+
+for(var index of array){
+    console.log(index)
+}
+*/
+

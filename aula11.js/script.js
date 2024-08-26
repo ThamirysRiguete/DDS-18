6d30b61b3583b08ff4c282d911a906721efc16d9
Thamirys Riguete <thamirys.nascimento@aluno.senai.br>
2024-08-26 14:38:58 -0300

    sa1

---
+++ aula11.js/script.js
@@ -0,0 +1,27 @@
+/*var alunos = ["Pedro", "Tiago", "João"]
+var notas = [7,8,9]
+
+console.log(aluno[2], "=", notas[2])
+
+//Declarando um objeto
+var aluno= {
+    nome: "Pedro", nota: 8
+}
+
+console.log(aluno.nome)
+console.log(aluno.nome)
+console.log(aluno.nome, "-", aluno.nota)
+*/
+
+//Seleciando uma propriedade
+var pessoa = {
+    nome: "Maria",
+    idade: 20
+}
+
+console.log(pessoa)
+console.log(pessoa.nome);
+console.log(pessoa["idade"])
+
+//Adicionando propriedade com uma variável
+var novaProp = "sobrenome"
